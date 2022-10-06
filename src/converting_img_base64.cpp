6ca69ba72999f246e940e2862510efc72046285b
terrificmn <mildsm@gmail.com>
2022-10-06 12:01:10 +0900

    line breaks test

--- src/converting_img_base64.cpp
+++ src/converting_img_base64.cpp
@@ -16,6 +16,7 @@ int main(int argc, char** argv) {
     std::ofstream output_pgm(filepath + "/maps/decoded_map.pgm");
 
     if(input.is_open()) {
+        int break_point_cnt = 0;
         while(getline(input, line)) {
             
             std::string encoded_str = base64_encode(reinterpret_cast<const unsigned char*>(line.c_str()), line.length());
@@ -26,8 +27,19 @@ int main(int argc, char** argv) {
                 all_tests_passed = false;
             }
 
-            output << encoded_str + "\n";  // add linebreak
-            output_pgm << decoded_str + "\n"; // add linebreak  // 복원했을 때 이미지가 다시 제대로 표현 안되는 문제, line 들이 엔터가 안되는 현상
+            /// 필요없는 기능 ,,, 하지만 char 와 const char 포인터와 비교할 경우 알아두기 참고            
+            // int last_of_array = encoded_str.size();
+            // ROS_INFO("encoded_size: %d", encoded_str.size());
+            // ROS_INFO("%d", encoded_str[last_of_array - 1]);
+            // char lastword = encoded_str[last_of_array -1];
+            // if (lastword == *("=")) {
+            //     ROS_INFO("= catched");
+            //     break_point_cnt++;
+            // }
+
+            output << encoded_str;   
+            // client에서 =기호가 호환이 안 되는 것 같아서 줄 바꿈시에 = 기호 제거 // base64_encode() 함수 내용 수정 -- converting_base64 pkg
+            output_pgm << decoded_str + "\n"; // add linebreak  // 복원했을 때 이미지가 다시 제대로 표현 안되는 문제, line 들이 엔터가 안되는 현상    
         }
         input.close();
     }
